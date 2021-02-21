11da45b63560d7aa5a33add839dc7787e292c288
Matt Padgett <mattpadgett@outlook.com>
2021-02-20 23:43:25 -0600

    Added basic class

---
+++ src/UsingProcessing.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class UsingProcessing extends PApplet {
+
+    // The argument passed to main must match the class name
+    public static void main(String[] args) {
+        PApplet.main("UsingProcessing");
+    }
+
+    // method used only for setting the size of the window
+    public void settings(){
+    	size(400, 400);
+    }
+
+    // identical use to setup in Processing IDE except for size()
+    public void setup(){
+        
+    }
+
+    // identical use to draw in Prcessing IDE
+    public void draw(){
+
+    }
+}
